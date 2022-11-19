8a35fada975065b4ad1d1123bbadcbff1feae874
Raihan Bin Solaiman <arraihan4897@gmail.com>
2022-11-19 10:24:23 +0600

    df

---
+++ function-recap.js
@@ -0,0 +1,17 @@
+function Average (ass1, ass2,ass3) {
+
+    const total = ass1+ ass2 +ass3;
+    const avg = total /3 ;
+    return avg
+
+
+    
+}
+
+const ass1Marks= 57;
+const ass2Marks=60;
+const ass3Marks=60;
+
+var cal =  Average (ass1Marks ,ass2Marks, ass3Marks) ;
+console.log( ' my average ',cal);
+
---
+++ m-19.js
@@ -0,0 +1,13 @@
+function bribgSingara (money ) {
+var singaraPrice  = 10;
+var quantity = money / singaraPrice
+return quantity
+
+}
+var myTaka = 150;
+
+const funct = bribgSingara(myTaka);
+console.log(funct);
+
+
+
