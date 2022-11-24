400421e458fe9704313edfd2cda161d08007e978
Raihan Bin Solaiman <arraihan4897@gmail.com>
2022-11-24 11:53:16 +0600

    max num

---
+++ m-21.js
@@ -0,0 +1,8 @@
+const lyrics = ' amk amar moto thakte daw ami nijk  nijer moto guchie niyechi '
+
+const doesExist = lyrics.includes('amk');
+const parts = lyrics.split(' ');
+const slice  = lyrics.slice(5 ,8);
+const join   = lyrics.join(':');
+console.log(doesExist);
+console.log(join);  
---
+++ problem.js
@@ -0,0 +1,16 @@
+const  jim =90 ; 
+const  del = 80 ; 
+const  cala =100;
+
+if (jim > del && jim > cala) {
+    console.log('jim pabe')
+   
+    
+} 
+else if ( del > jim  && del > cala)  {
+        console.log('del pabe');
+    }
+    else {
+        console.log('cala pabe');
+
+    }
