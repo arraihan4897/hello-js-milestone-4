859b43bc4bbecc925f831953c0a95d31325b266e
Raihan Bin Solaiman <arraihan4897@gmail.com>
2022-11-24 09:22:10 +0600

    add & mul

---
+++ cal-factorial.js
@@ -0,0 +1,30 @@
+// mul ++ for
+function getmulSum () {
+    let mulsum= 1;
+    for (let i=1 ;i <=7; i++){
+        mulsum = mulsum *i 
+      console.log( i, mulsum );
+  
+  }
+  return mulsum;
+    
+}
+const mulcal = getmulSum();
+console.log(mulcal);
+
+
+// add++ for
+
+
+function getSum () {
+    let sum= 0;
+    for (let i=0 ;i <=7; i++){
+    sum = sum +i 
+      console.log( i, sum );
+  
+  }
+  return sum;
+    
+}
+const cal = getSum();
+console.log(cal);
